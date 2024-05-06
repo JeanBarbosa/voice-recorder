74bac86f80118a501fca4502dac6aa44fc03a9ec
jeanbarbosa <programmer.jean@gmail.com>
2024-05-06 20:13:18 -0300

    chore: create component input

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native";
+import { Input } from "../components/input";
 
 export default function Home() {
   return (
     <View className="flex-1 bg-black items-center justify-center">
       <Text className="text-2xl text-white"> Ola, mundo! </Text>
+      <Input>
+        <Input.Field placeholder="nome" />
+      </Input>
     </View>
   );
 }
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,8 +1,8 @@
 export const colors = {
-    white: "#e1e1e6",
+  white: "#e1e1e6",
 
-    gray: {
-        200: "c4c4cc",
-        300: "8d8d99",
-    },
-}
+  gray: {
+    200: "#c4c4cc",
+    300: "#8d8d99",
+  },
+};
